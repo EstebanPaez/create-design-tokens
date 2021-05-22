cafe4059a190b59a92ee8b3ed8a2a13a9706608e
Camilo Paez <cepn26@gmail.com>
2021-05-22 13:12:09 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Create-design-tokens
+
+Get a design tokens template based on Tailwind CSS default config.
+
+## Usage
+
+Run:
+
+```shell
+npx create-design-tokens
+```
---
+++ index.js
@@ -0,0 +1,8 @@
+const axios = require('axios').default
+const fs = require('fs').promises
+const path = require('path')
+
+module.exports = async () => {
+  const { data } = await axios.get('https://gist.githubusercontent.com/EstebanPaez/b073de6fa717a384f989ceadb6d44c41/raw/8eb7f9cf71f7e2ba2056632c9edd840fdd95b6a4/tokensBase.js')
+  return fs.writeFile(path.join(process.cwd(), 'designTokens.js'), data, 'utf-8') 
+}
