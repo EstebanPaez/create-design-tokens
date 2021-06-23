c5e47eea19a216ff1d9e1ba6e41f23c157e4cc76
Camilo Paez <cepn26@gmail.com>
2021-06-22 22:29:55 -0500

    Change username

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const fs = require('fs').promises
 const path = require('path')
 
 module.exports = async () => {
-  const { data } = await axios.get('https://gist.githubusercontent.com/CamiloEPaez/b073de6fa717a384f989ceadb6d44c41/raw/8eb7f9cf71f7e2ba2056632c9edd840fdd95b6a4/tokensBase.js')
+  const { data } = await axios.get('https://gist.githubusercontent.com/camilopaezz/b073de6fa717a384f989ceadb6d44c41/raw/8eb7f9cf71f7e2ba2056632c9edd840fdd95b6a4/tokensBase.js')
   return fs.writeFile(path.join(process.cwd(), 'designTokens.js'), data, 'utf-8') 
 }
